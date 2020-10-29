e53ab7f6e3600dc43b235fcd1ee11374296d0fe1
Sofya Kholodkova <reima.cx@gmail.com>
2020-10-29 11:43:07 +0300

    Refactor task_2.py

--- Homeworks/lesson_4/task_2.py
+++ Homeworks/lesson_4/task_2.py
@@ -10,4 +10,4 @@
 
 numbers = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
-print(*[num for i, num in enumerate(numbers) if i > 0 and num > numbers[i - 1]])
+print(*[num for i, num in enumerate(numbers) if i and num > numbers[i - 1]])
