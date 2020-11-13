e17d4d3b9d25ecf922d49e506a8333ab29473158
Sofya Kholodkova <reima.cx@gmail.com>
2020-11-13 19:21:37 +0300

    Fix task_7.py

--- Homeworks/lesson_5/task_7.py
+++ Homeworks/lesson_5/task_7.py
@@ -15,16 +15,27 @@ class ComplexNumber:
     def __str__(self):
         return f'{self.real_part} + {self.imaginary_part}i'
 
-    def __add__(self, other):
-        return ComplexNumber(self.real_part + other.real_part, self.imaginary_part + other.imaginary_part)
+    def __add__(self, other: 'ComplexNumber'):
+        return ComplexNumber(
+            self.real_part + other.real_part,
+            self.imaginary_part + other.imaginary_part
+        )
 
-    def __mul__(self, other):
-        return ComplexNumber((self.real_part * other.real_part) - (self.imaginary_part * other.imaginary_part),
-                             self.real_part * other.imaginary_part + self.imaginary_part * other.real_part)
+    def __mul__(self, other: 'ComplexNumber'):
+        return ComplexNumber(
+            (self.real_part * other.real_part) - (self.imaginary_part * other.imaginary_part),
+            (self.real_part * other.imaginary_part) + (self.imaginary_part * other.real_part)
+        )
+
+    def __eq__(self, other: 'ComplexNumber'):
+        return self.real_part == other.real_part and self.imaginary_part == other.imaginary_part
 
 
 n1 = ComplexNumber(5, 2)
 n2 = ComplexNumber(5, 1)
 
-print(n1 + n2)
-print(n1 * n2)
+assert n1 + n2 == ComplexNumber(10, 3)
+assert n1 * n2 == ComplexNumber(23, 15)
+
+print(f'({n1}) + ({n2}) = {n1 + n2}')
+print(f'({n1}) * ({n2}) = {n1 * n2}')
